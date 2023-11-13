50f6cbe717b6c233d41c9088d2b0b52daca59c4d
YAOJaredz <yyao01@wesleyan.edu>
2023-11-12 22:16:30 -0500

    created config file

---
+++ config/card_deal.py
@@ -0,0 +1,4 @@
+from data_processing.database import Card, CardDatabase
+
+def deal_cards(database: CardDatabase) -> None:
+    pass
---
+++ config/config.py
@@ -0,0 +1,30 @@
+class config():
+    def __init__(self, **kwargs) -> None:
+        self.num_rounds = kwargs['num_rounds']
+
+        # Card deck
+        self.num_decks = kwargs['num_decks']
+        self.joker = kwargs['joker']
+        self.order = kwargs['order']
+        self.initial_hand = kwargs['initial_hand']
+
+        # Card deal
+        self.num_cards_per_round = kwargs['num_cards_per_round']
+        self.draw_flag = kwargs['draw_flag']
+        self.num_draw = kwargs['num_draw']
+        
+        # Community cards
+        self.comm_flag = kwargs['comm_flag']
+        self.first_round = kwargs['first_round']
+        self.first_round_num = kwargs['first_round_num']
+        self.num_added_per_round = kwargs['num_added_per_round']
+
+        # Card play
+        self.play_flag = kwargs['play_flag']
+        self.num_cards_played_per_round = kwargs['num_cards_played_per_round']
+
+        # Multiuser
+        self.multiuser_flag = False
+
+        # Bid
+        self.bid_flag = False
