dfe5490dd22e64f820421941ba7fecf91f65b4b0
YAOJaredz <yyao01@wesleyan.edu>
2023-11-12 23:13:59 -0500

    added test config

--- config/config.py
+++ config/config.py
@@ -1,30 +1,30 @@
 class Config():
     def __init__(self, **kwargs) -> None:
-        self.num_rounds = kwargs['num_rounds']
+        self.num_rounds: int = kwargs['num_rounds']
 
         # Card deck
-        self.num_decks = kwargs['num_decks']
-        self.joker = kwargs['joker']
-        self.order = kwargs['order']
+        self.num_decks: int = kwargs['num_decks']
+        self.joker: bool = kwargs['joker']
+        self.order: int = kwargs['order']
 
         # Card deal
-        self.initial_hand = kwargs['initial_hand']
-        self.num_cards_per_round = kwargs['num_cards_per_round']
-        self.draw_flag = kwargs['draw_flag']
-        self.num_draw = kwargs['num_draw']
+        self.num_initial_hand: int = kwargs['num_initial_hand']
+        self.num_cards_dealt_per_round: int = kwargs['num_cards_dealt_per_round']
+        self.draw_flag: bool = kwargs['draw_flag']
+        self.num_draw: int = kwargs['num_draw']
         
         # Community cards
-        self.comm_flag = kwargs['comm_flag']
-        self.first_round = kwargs['first_round']
-        self.first_round_num = kwargs['first_round_num']
-        self.num_added_per_round = kwargs['num_added_per_round']
+        self.comm_flag: bool = kwargs['comm_flag']
+        self.first_round_of_comm: int = kwargs['first_round_of_comm']
+        self.first_round_num_comm: int = kwargs['first_round_num_comm']
+        self.num_added_comm_per_round: int = kwargs['num_added_comm_per_round']
 
         # Card play
-        self.play_flag = kwargs['play_flag']
-        self.num_cards_played_per_round = kwargs['num_cards_played_per_round']
+        self.play_flag: bool = kwargs['play_flag']
+        self.num_cards_played_per_round: int = kwargs['num_cards_played_per_round']
 
         # Multiuser
-        self.multiuser_flag = False
+        self.multiuser_flag: bool = False
 
         # Bid
-        self.bid_flag = False
+        self.bidding_flag: bool = False
