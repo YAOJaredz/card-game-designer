990ac9be0f167d02ba0b1fb4758fe9a0f715ce1b
Simin Liu <sliu02@wesleyan.edu>
2023-11-24 16:41:39 -0500

    Add a test

--- operations/card_play.py
+++ operations/card_play.py
@@ -4,7 +4,6 @@ sys.path.append('.')
 
 from data_processing.database import Card, CardDatabase
 from operations.config import Config
-import operations.card_deck as deck
 
 def get_identifier(play_cards:list[int]) -> list[int]:
     """Get the identifiers of cards.
@@ -17,20 +16,6 @@ def get_identifier(play_cards:list[int]) -> list[int]:
     for card in play_cards:
         identifiers.add(card.identifier)
     return identifiers
-        
-def find_card(database: CardDatabase, player:str, identifier: int) -> Card:
-    """Find the card according to the identifier.
-    Assumption: The identifier is in hand. 
-    Args:
-        database (CardDatabase): The card database.
-        player (str): The player who is playing the cards.
-        identifier (int): The identifier of the card to be found.
-    Return:
-        Card: The card to be found.
-    """
-    for card in database.hands[player]:
-        if card.identifier==identifier:
-            return card
 
 def play_cards(player: str, play_cards: list[int], database: CardDatabase, round: int, config: Config) -> CardDatabase:
     """
@@ -50,9 +35,42 @@ def play_cards(player: str, play_cards: list[int], database: CardDatabase, round
         raise ValueError('Exceeds the number of cards to be played.')
     elif set(play_cards).issubset(get_identifier(database.hands[player])) == False:
         raise ValueError('Cards not in hand.')
+    elif player not in database.players:
+        raise ValueError('Player not in the game.')
     else:
         for card_id in play_cards:
-            card=find_card(database, player, card_id)
+            card=database.find_card(player, card_id)
             database.discard.add(card)
             database.hands[player].remove(card)
     return database
+
+if __name__ == "__main__":
+    from card_deck import initialization
+    from card_deck import card_print_all
+
+    test_config_path = 'save/test_config.json'
+    config = Config(**json.load(open(test_config_path, 'r')))
+
+    database = initialization(config)
+    database.players = ['A']
+    
+    #Give a card 1, 2, 3
+    database.hands['A']=[]
+    database.hands['A'].append(database.deck[0])
+    database.hands['A'].append(database.deck[1])
+    database.hands['A'].append(database.deck[2])
+    database.deck=database.deck[3:]
+    # a should have card 1, 2, 3
+    print("A has cards:")
+    card_print_all(database.hands["A"])
+    print("Deck should start from card 4")
+    # deck should start from card 4
+    card_print_all(database.deck)
+    print("------ Card Play: A plays card 1 ------")
+    play_cards("A", [1], database, 1, config)
+    print("After play, A has cards:")
+    card_print_all(database.hands["A"])
+    print("Deck:")
+    card_print_all(database.deck)
+    print("Discard should have card 1")
+    card_print_all(database.discard)
