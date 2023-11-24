9e78cdaa050b491fa6db19d4d518a341d38bc411
YAOJaredz <yyao01@wesleyan.edu>
2023-11-24 01:00:46 -0500

    implemented community card rule

--- operations/community_card.py
+++ operations/community_card.py
@@ -17,4 +17,27 @@ def update_community(database: CardDatabase, round: int, config: Config) -> Card
     Return:
         CardDatabase: The updated card database.
     """
+    if not config.comm_flag:
+        return database
+    
+    if round == config.first_round_of_comm:
+        database.community = database.pop_from_deck(config.first_round_num_comm)
+    elif round > config.first_round_of_comm:
+        database.community.extend(database.pop_from_deck(config.num_added_comm_per_round))
     return database
+
+if __name__ == '__main__':
+    from card_deck import initialization
+
+    config = Config(**json.load(open('save/test_config.json','r')))
+
+    print(config.first_round_of_comm)
+
+    database = initialization(config)
+    print(database.community)
+    database = update_community(database, 1, config)
+    print(database.community)
+    database = update_community(database, 2, config)
+    print(database.community)
+    database = update_community(database, 3, config)
+    print(database.community)
