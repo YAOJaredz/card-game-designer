4a5755e804fd64b3043232fc0ed164a1c9e98570
YAOJaredz <yyao01@wesleyan.edu>
2023-12-15 12:55:32 -0500

    Update wait time for computer player

--- user_scripts/cp_strategy_template.py
+++ user_scripts/cp_strategy_template.py
@@ -33,19 +33,21 @@ class ComputerPlayer:
         The strategy of the computer player.
         User can use round, community, cards_recently_played to develop their own computer player strategy. 
         As a default in the program, the computer player will randomly choose cards to play.
+
         Args:
             round (int): The current round.
             hand (list[Card]): The cards in the hand.
             community (list[Card]): The community cards.
             num_cards_played_per_round (int): The number of cards to be played per round.
             cards_recently_played (list[Card]): The cards that are recently played.
-        Return:
+
+        Returns:
             list[Card]: The cards to be played.
         """
         self.hand = hand
         # num_cards_played_per_round == -1 means unlimited number of cards to play, then randomly choose a number of cards to play. 
         if num_cards_played_per_round == -1:
-            num_play=random.randint(0, len(self.hand))
+            num_play = random.randint(1, min(len(self.hand),5))
             card_to_play = random.sample(self.hand, num_play)
         else: 
             card_to_play = random.sample(self.hand, min(num_cards_played_per_round, len(self.hand)))
@@ -69,7 +71,8 @@ class ComputerPlayer:
             community (list[Card]): The current community cards.
             repetitive_draw_flag (bool): Whether or not to allow repetitive draw.
             num_cards_drawn_per_time (int): The number of cards to be drawn per round.
-        Return:
+
+        Returns:
             int: The number of times to draw cards.
         """
         return 1
